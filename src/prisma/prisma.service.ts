27dccf3b84d723eb7322150cd1480dc3e1d5a870
mirzabekov00 <prog.eldar@gmail.com>
2022-07-25 20:43:43 +0300

    feat(auth): inital auth, prisma services

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+  }
+
+  onModuleInit() {
+    return null;
+  }
+
+  onModuleDestroy() {
+    return null;
+  }
+}
