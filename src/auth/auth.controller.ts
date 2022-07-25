b3aed6f0b672158ffe8c40062da78dffadd05d93
mirzabekov00 <prog.eldar@gmail.com>
2022-07-25 21:07:09 +0300

    feat(auth): initial auth service, add class-validator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() authDto: AuthDto) {
+    return this.authService.register(authDto);
+  }
+
+  @Post('login')
+  login() {
+    return this.authService.login();
+  }
+
+  @Post('logout')
+  logout() {
+    return this.authService.logout();
+  }
+
+  @Post('refresh')
+  refresh() {
+    return this.authService.refresh();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async register(authDto: AuthDto) {
+    // const newUser = await this.prismaService.user.create({
+    //   data: {
+    //     email: authDto.email,
+    //   },
+    // });
+  }
+
+  async login() {
+    return null;
+  }
+
+  async logout() {
+    return null;
+  }
+
+  async refresh() {
+    return null;
+  }
+}
